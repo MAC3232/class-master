84e7023e9f441be1fbe3a8314d1c033efa7dd93b
Julio Cesar <108636544+JuliCesar2022@users.noreply.github.com>
2024-11-15 03:17:02 -0500

    update dockerfile 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-# Asegurarse de que el archivo .env exista
-if [ ! -f "/var/www/.env" ]; then
-  echo ".env file not found!"
-  exit 1
-fi
 
-# Esperar a que el servicio de base de datos esté disponible
-echo "Esperando a que MySQL esté disponible..."
-until nc -z -v -w30 $DB_HOST $DB_PORT
-do
-  echo "Esperando a MySQL en el host $DB_HOST:$DB_PORT..."
-  sleep 5
-done
 
 # Generar la clave de la aplicación
 
