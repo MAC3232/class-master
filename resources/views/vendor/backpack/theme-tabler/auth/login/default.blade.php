ad7fbfb5b4f320a4de38735cc475a19893035c52
Julio Cesar <juliobonifacio53@gmail.com>
2025-01-21 08:31:43 -0500

    change icon

--- resources/views/vendor/backpack/theme-tabler/auth/login/default.blade.php
+++ resources/views/vendor/backpack/theme-tabler/auth/login/default.blade.php
@@ -7,12 +7,12 @@
                 <!-- {!! backpack_theme_config('project_logo') !!} -->
             </div>
             <div style="display: flex; justify-content: center; align-items: center;" class="card card-md">
-                <img style="width: 15rem;"  src="{{asset('img/icon.png')}}" alt="">
+                <img style="width: 20rem; margin:10px;margin-top:20px"  src="{{asset('img/icon.png')}}" alt="">
                 <div class="card-body pt-0">
                     @include(backpack_view('auth.login.inc.form'))
                 </div>
             </div>
-         
+
         </div>
     </div>
 @endsection
