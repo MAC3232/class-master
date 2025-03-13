b6f4056bdca405f7014a54493f0e579bf537251b
unknown <luisladeuth567@gmail.com>
2025-03-13 02:06:43 -0500

    The Search section was modified by adding a new animation. The cards were also modified so that only the teacher has the option to see them and their respective functionality (paging not finished).

--- resources/views/courses/listCourses.blade.php
+++ resources/views/courses/listCourses.blade.php
@@ -29,7 +29,7 @@
         justify-content: space-between;
         align-items: center;
         margin-bottom: 20px;
-        margin-top: 30px;
+        margin-top: 2px;
         gap:0.5rem;
         position: relative; 
     }
@@ -39,13 +39,12 @@
         padding: 8px 12px 8px 35px; 
         border-radius: 5px;
         width: 100%;
-        background-color: #1F2937;
-        border: 2px solid #374151;
-        color: white; 
+        border: 2px solid #E7E6E7;
         position: relative; 
     }
     .search-input:focus{
-        border: 2px solid blue;
+        border: 2px solid #ADA1F5;  /* Color del borde */
+        box-shadow: 0 0 5px #DED9FB; /* Color de la sombra */
         outline: none;
     }
 
@@ -70,63 +69,12 @@
         position: relative; 
      }
 
-     /* Botón de Filtro */
-    .filter-button {
-        padding: 8px 16px;
-        background-color: #007bff;
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-        width:10rem;
-        position: relative; 
-    }
-
-    /* Dropdown de Filtros */
-    .filter-dropdown-menu {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        background-color: #374151;
-        border-radius: 5px;
-        overflow: hidden;
-        display: none; 
-        position: absolute; 
-        top: 100%; 
-        right:0; 
-        width: auto; 
-        min-width: 10rem;
-        z-index: 110; 
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    }
-
-
-    .filter-dropdown-menu.show {
-        display: block; 
-    }
-
-    .filter-dropdown-item {
-        padding: 10px 15px;
-        cursor: pointer;
-        color: white;
-        text-align: left; 
-        white-space: nowrap; 
-    }
-
-    .filter-dropdown-item:hover {
-        background-color: #4B5563;
-    }
-
-    .filter-dropdown-item:not(:last-child) {
-        border-bottom: 1px solid #4B5563;
-    }
-
-
     .card-container {
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(250px, 300px)); 
         gap: 20px;
         justify-content: center;
+        position: relative;
     }
 
     .card:hover{
@@ -134,6 +82,7 @@
         box-shadow: 0 8px 12px rgba(0, 0, 0, 0.5); 
         transition: transform 0.3s ease-in-out;
     }
+    
 
     .card {
        max-width: 300px; 
@@ -149,6 +98,7 @@
        position: relative;
        background-color: #1F2937; 
        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3); 
+       background-color: #4c6ef5; 
     }
 
     .card-icon-container {
@@ -226,54 +176,233 @@
     .card-dropdown-item:not(:last-child) {
         border-bottom: 1px solid #4B5563;
     }
-    .loading-indicator {
-    text-align: center;
-    font-size: 1.2rem;
-    color: #0D8C81;
-    padding: 20px;
-    width: 100%;
+
+    .spinner {
+    font-size: 28px;
+    position: relative;
+    display: inline-block;
+    width: 1em;
+    height: 1em;
 }
+
+.spinner.center {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+}
+
+.spinner .spinner-blade {
+    position: absolute;
+    left: 0.4629em;
+    bottom: 0;
+    width: 0.074em;
+    height: 0.2777em;
+    border-radius: 0.0555em;
+    background-color: #0D8C81; /* Color del blade */
+    -webkit-transform-origin: center -0.2222em;
+    -ms-transform-origin: center -0.2222em;
+    transform-origin: center -0.2222em;
+    animation: spinner-fade9234 0.4s infinite linear;
+}
+
+.spinner .spinner-blade:nth-child(1) {
+    -webkit-animation-delay: 0s;
+    animation-delay: 0s;
+    -webkit-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    transform: rotate(0deg);
+}
+
+.spinner .spinner-blade:nth-child(2) {
+    -webkit-animation-delay: 0.083s;
+    animation-delay: 0.083s;
+    -webkit-transform: rotate(30deg);
+    -ms-transform: rotate(30deg);
+    transform: rotate(30deg);
+}
+
+.spinner .spinner-blade:nth-child(3) {
+    -webkit-animation-delay: 0.166s;
+    animation-delay: 0.166s;
+    -webkit-transform: rotate(60deg);
+    -ms-transform: rotate(60deg);
+    transform: rotate(60deg);
+}
+
+.spinner .spinner-blade:nth-child(4) {
+    -webkit-animation-delay: 0.249s;
+    animation-delay: 0.249s;
+    -webkit-transform: rotate(90deg);
+    -ms-transform: rotate(90deg);
+    transform: rotate(90deg);
+}
+
+.spinner .spinner-blade:nth-child(5) {
+    -webkit-animation-delay: 0.332s;
+    animation-delay: 0.332s;
+    -webkit-transform: rotate(120deg);
+    -ms-transform: rotate(120deg);
+    transform: rotate(120deg);
+}
+
+.spinner .spinner-blade:nth-child(6) {
+    -webkit-animation-delay: 0.415s;
+    animation-delay: 0.415s;
+    -webkit-transform: rotate(150deg);
+    -ms-transform: rotate(150deg);
+    transform: rotate(150deg);
+}
+
+.spinner .spinner-blade:nth-child(7) {
+    -webkit-animation-delay: 0.498s;
+    animation-delay: 0.498s;
+    -webkit-transform: rotate(180deg);
+    -ms-transform: rotate(180deg);
+    transform: rotate(180deg);
+}
+
+.spinner .spinner-blade:nth-child(8) {
+    -webkit-animation-delay: 0.581s;
+    animation-delay: 0.581s;
+    -webkit-transform: rotate(210deg);
+    -ms-transform: rotate(210deg);
+    transform: rotate(210deg);
+}
+
+.spinner .spinner-blade:nth-child(9) {
+    -webkit-animation-delay: 0.664s;
+    animation-delay: 0.664s;
+    -webkit-transform: rotate(240deg);
+    -ms-transform: rotate(240deg);
+    transform: rotate(240deg);
+}
+
+.spinner .spinner-blade:nth-child(10) {
+    -webkit-animation-delay: 0.747s;
+    animation-delay: 0.747s;
+    -webkit-transform: rotate(270deg);
+    -ms-transform: rotate(270deg);
+    transform: rotate(270deg);
+}
+
+.spinner .spinner-blade:nth-child(11) {
+    -webkit-animation-delay: 0.83s;
+    animation-delay: 0.83s;
+    -webkit-transform: rotate(300deg);
+    -ms-transform: rotate(300deg);
+    transform: rotate(300deg);
+}
+
+.spinner .spinner-blade:nth-child(12) {
+    -webkit-animation-delay: 0.913s;
+    animation-delay: 0.913s;
+    -webkit-transform: rotate(330deg);
+    -ms-transform: rotate(330deg);
+    transform: rotate(330deg);
+}
+
+@keyframes spinner-fade9234 {
+    0% {
+        background-color: #69717d;
+    }
+
+    100% {
+        background-color: transparent;
+    }
+}
+
+.loading-indicator {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); /* Centra el loading-indicator */
+    z-index: 1000; /* Asegura que esté encima de otros elementos */
+}
+
+.p-4.shadow-sm.bg-light.mt-4{
+    border-radius: 5px;
+}
+.pagination-container {
+    text-align: right;
+    margin-top: 20px;
+}
+
+   /* ... Pagination :D ... */
+   
+.pagination-container button {
+    padding: 8px 12px;
+    margin: 0 5px;
+    border: 1px solid #ddd;
+    background-color: #f0f0f0;
+    color: #333;
+    cursor: pointer;
+    border-radius: 4px;
+}
+
+.pagination-container button:disabled {
+    background-color: #ddd;
+    color: #999;
+    cursor: default;
+}
+
+
 
 </style>
 @endpush
 
 @section('content')
-<div class="search-filter-container">
-    <input type="text" class="search-input" placeholder="Buscar Asignatura..." onkeyup="searchCourses()">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="search-icon"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
-    <button class="filter-button" onclick="toggleFilterDropdown()">Filtros</button>
-    <ul class="filter-dropdown-menu" id="filterDropdownMenu">
-        <li class="filter-dropdown-item">Prueba Filtro 1</li>
-        <li class="filter-dropdown-item">Prueba Filtro 2</li>
-        <li class="filter-dropdown-item">Prueba Filtro 3</li>
-    </ul>
-    <button class="create-button" onclick="createCourse()">Crear</button> {{-- Botón de Crear --}}
-</div>
-
-<div class="p-4 shadow-sm bg-light">
-    <div class="card-container">
-        <div class="loading-indicator" style="display: none;">Buscando...</div>
+<div class="p-4 shadow-sm bg-light mt-4">
+    <div class="search-filter-container">
+        <input type="text" class="search-input" placeholder="Buscar Asignatura..." onkeyup="searchCourses()">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#9CA3AF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="search-icon"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
+        <button class="create-button" onclick="createCourse()">Crear Asignatura</button> {{-- Botón de Crear --}}
     </div>
+    <div class="card-container">
+        <div class="loading-indicator" style="display: none;">
+            <div class="spinner center">
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+            </div>
+        </div>  
+    </div>
+    <div class="no-results-message" style="display: none; text-align: center; width: 100%; font-size: 1.2em;">
+        No hay asignaturas D:
+    </div>
+    <div class="pagination-container" style="text-align: right; margin-top: 20px;"></div>
 </div>
-
 @endsection
 
 @push('after_scripts')
 <script>
-    let cardsData = [];
-   
-    $.ajax({
-        url: '/admin/searchCourses',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            console.log('Datos recibidos:', data);
-            cardsData = data;
-            renderCards(cardsData);
-        },
-    });
-    function searchCourses() {
-    let searchQuery = $('.search-input').val().trim(); 
+let cardsData = [];
+
+$.ajax({
+    url: '/admin/searchCourses',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+        console.log('Datos recibidos:', data);
+        cardsData = data;
+        renderCards(cardsData);
+    },
+});
+
+function searchCourses() {
+    console.log("ejecutando searchCourses");
+    let searchQuery = $('.search-input').val().trim();
 
     $('.loading-indicator').show(); // Mostrar el indicador de carga
     $('.card-container .card').remove(); // Eliminar tarjetas antiguas mientras se carga
@@ -285,20 +414,28 @@
         dataType: 'json',
         success: function (data) {
             $('.loading-indicator').hide(); // Ocultar el indicador de carga
-            renderCards(data); 
+
+            if (data.length === 0) {
+                // Si no hay resultados, oculta el card-container y muestra el mensaje
+                $('.card-container').hide();
+                $('.no-results-message').show();
+            } else {
+                // Si hay resultados, muestra el card-container y oculta el mensaje
+                $('.card-container').show();
+                $('.no-results-message').hide();
+                renderCards(data);
+            }
         },
         error: function () {
-            $('.loading-indicator').text("Error al cargar datos").show();
+            $('.loading-indicator').text("Error").show();
         }
     });
 }
 
-
-
-    function renderCards(cards) {
+function renderCards(cards) {
     let cardContainer = $('.card-container');
 
-    
+    // Ya no necesitas limpiar el contenedor aquí
     let existingCards = cardContainer.children('.card');
 
     cards.forEach(function(card, index) {
@@ -310,12 +447,14 @@
         } else {
             // Si la tarjeta no existe, crea una nueva y la agrega al container
             let cardHtml = `
-                <div class="card" style="background-color: ${getCardColor(index)};">
+                <div class="card" data-id="${card.id} style="background-color: ${getCardColor(index)};">
                     <div class="card-dropdown-container">
                         <ul class="card-dropdown-menu" id="cardDropdownMenu${index}">
-                            <li class="card-dropdown-item" onclick="cardActionView()">Ver</li>
+                            <li class="card-dropdown-item" onclick="cardActionView(${card.id})">Ver</li>
+                            @if (backpack_user()->hasRole('admin'))
                             <li class="card-dropdown-item" onclick="cardActionEdit()">Editar</li>
                             <li class="card-dropdown-item" onclick="cardActionDelete()">Eliminar</li>
+                            @endif
                         </ul>
                     </div>
                     <div class="options-icon" onclick="toggleDropdown('cardDropdownMenu${index}')">
@@ -334,6 +473,10 @@
                 </div>
             `;
             cardContainer.append(cardHtml);
+            let newCard = cardContainer.children('.card').last();
+            newCard.click(function() {
+                cardActionView(card.id);
+            });
         }
     });
 
@@ -343,80 +486,51 @@
     }
 }
 
-    // Agrega el color de Fondo para todas las tarjetas.
-    function getCardColor(index) {
-        const colors = ["#4c6ef5"];
-        return colors[index % colors.length];
+// Agrega el color de Fondo para todas las tarjetas.
+function getCardColor(index) {
+    return "#4c6ef5"; 
+}
+
+function toggleDropdown(dropdownId) {
+    const dropdownMenu = document.getElementById(dropdownId);
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+
+        document.querySelectorAll('.card-dropdown-menu.show').forEach(function(otherDropdown) {
+            if (otherDropdown.id !== dropdownId) {
+                otherDropdown.style.display = 'none';
+                otherDropdown.classList.remove('show');
+            }
+        });
     }
+    dropdownMenu.classList.toggle('show');
+}
 
-    function toggleDropdown(dropdownId) {
-        const dropdownMenu = document.getElementById(dropdownId);
-        if (dropdownMenu.style.display === 'block') {
-            dropdownMenu.style.display = 'none';
-        } else {
-            dropdownMenu.style.display = 'block';
+window.onclick = function(event) {
+    const isClickInsideCardDropdown = event.target.closest('.card-dropdown-menu');
+    const isClickOnOptionsIcon = event.target.closest('.options-icon');
 
-            document.querySelectorAll('.card-dropdown-menu.show').forEach(function(otherDropdown) {
-                if (otherDropdown.id !== dropdownId) {
-                    otherDropdown.style.display = 'none';
-                    otherDropdown.classList.remove('show');
-                }
-            });
-        }
-        dropdownMenu.classList.toggle('show');
+    if (!isClickInsideCardDropdown && !isClickOnOptionsIcon) {
+        document.querySelectorAll('.card-dropdown-menu.show').forEach(function(openDropdown) {
+            openDropdown.classList.remove('show');
+            openDropdown.style.display = 'none';
+        });
     }
-
-    function toggleFilterDropdown() {
-        const filterDropdownMenu = document.getElementById('filterDropdownMenu');
-        if (filterDropdownMenu.style.display === 'block') {
-            filterDropdownMenu.style.display = 'none';
-        } else {
-            filterDropdownMenu.style.display = 'block';
-
-            document.querySelectorAll('.filter-dropdown-menu.show').forEach(function(otherDropdown) {
-                if (otherDropdown.id !== 'filterDropdownMenu') {
-                    otherDropdown.style.display = 'none';
-                    otherDropdown.classList.remove('show');
-                }
-            });
-
-            document.querySelectorAll('.card-dropdown-menu.show').forEach(function(cardDropdown) {
-                cardDropdown.style.display = 'none';
-                cardDropdown.classList.remove('show');
-            });
-        }
-        filterDropdownMenu.classList.toggle('show');
-    }
-
-    window.onclick = function(event) {
-        const isClickInsideCardDropdown = event.target.closest('.card-dropdown-menu');
-        const isClickOnOptionsIcon = event.target.closest('.options-icon');
-        const isClickInsideFilterDropdown = event.target.closest('.filter-dropdown-menu');
-        const isClickOnFilterButton = event.target.closest('.filter-button');
-
-        if (!isClickInsideCardDropdown && !isClickOnOptionsIcon && !isClickInsideFilterDropdown && !isClickOnFilterButton) {
-            document.querySelectorAll('.card-dropdown-menu.show').forEach(function(openDropdown) {
-                openDropdown.classList.remove('show');
-                openDropdown.style.display = 'none';
-            });
-            document.querySelectorAll('.filter-dropdown-menu.show').forEach(function(openFilterDropdown) {
-                openFilterDropdown.classList.remove('show');
-                openFilterDropdown.style.display = 'none';
-            });
-        }
-    }
-
-    function cardActionView() {
-        alert('View action clicked');
-    }
-
-    function cardActionEdit() {
-        alert('Edit action clicked');
-    }
+}
 
 
-    function cardActionDelete() {
-        alert('Delete action clicked');
-    }
+function cardActionView(cardId) {
+    window.location.href = `/admin/asignaturas/${cardId}/show`;
+}
+
+function cardActionEdit() {
+    alert('Edit action clicked');
+}
+
+function cardActionDelete() {
+    alert('Delete action clicked');
+}
 </script>
 @endpush
