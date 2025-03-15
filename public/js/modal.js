97925ea0c98fd6c51eb3f12567a80b4040a8380b
JulioCes2025 <jbonifacio@glerp.net.co>
2025-03-15 17:50:35 -0500

    fix:correct buttons of add criteria and levels

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,227 +1,196 @@
+
+
+
 let currentMode = '';
 
-function showCustomModal(mode, criterio_id, nivel_id) {
+function showCustomModal(mode, criterio_id, nivel_id, descripcion_id) {
     currentMode = mode;
-    criterio = criterio_id;
-    nivel = nivel_id;
+    criterio = criterio_id
+    nivel = nivel_id
+    descripcion = descripcion_id
     document.getElementById("addElementForm").reset();
     document.getElementById("criteriaInputs").style.display = mode === 'criterion' ? 'block' : 'none';
     document.getElementById("levelInputs").style.display = mode === 'level' ? 'block' : 'none';
     document.getElementById("descripcionInputs").style.display = mode === 'descripcion' ? 'block' : 'none';
-
-
-
-    let currentMode = '';
-
-    function showCustomModal(mode, criterio_id, nivel_id, descripcion_id) {
-        currentMode = mode;
-        criterio = criterio_id
-        nivel = nivel_id
-        descripcion = descripcion_id
-        document.getElementById("addElementForm").reset();
-        document.getElementById("criteriaInputs").style.display = mode === 'criterion' ? 'block' : 'none';
-        document.getElementById("levelInputs").style.display = mode === 'level' ? 'block' : 'none';
-        document.getElementById("descripcionInputs").style.display = mode === 'descripcion' ? 'block' : 'none';
-        document.getElementById("levelInputsEdit").style.display = mode === 'update_modal_level' ? 'block' : 'none';
-        document.getElementById("descripcionInputsEdit").style.display = mode === 'update_modal_descripcion_criterio' ? 'block' : 'none';
-        document.getElementById("descripcionaInputUpdate").style.display = mode === 'update_modal_descripcion' ? 'block' : 'none';
+    document.getElementById("levelInputsEdit").style.display = mode === 'update_modal_level' ? 'block' : 'none';
+    document.getElementById("descripcionInputsEdit").style.display = mode === 'update_modal_descripcion_criterio' ? 'block' : 'none';
+    document.getElementById("descripcionaInputUpdate").style.display = mode === 'update_modal_descripcion' ? 'block' : 'none';
 
 
 
 
-        if (currentMode == 'calificar') {
-            document.getElementById("CalificarInputs").style.display = mode === 'calificar' ? 'block' : 'none';
-            document.getElementById("customModalLabel").innerText = 'Calificar al estudiante';
+    if (currentMode == 'calificar') {
+        document.getElementById("CalificarInputs").style.display = mode === 'calificar' ? 'block' : 'none';
+        document.getElementById("customModalLabel").innerText = 'Calificar al estudiante';
 
-        }
-        if(currentMode == 'update_modal_level'){
-            document.getElementById("customModalLabel").innerText = 'Editar nivel';
-        }
-        if(currentMode == 'update_modal_descripcion_criterio'){
-            document.getElementById("customModalLabel").innerText = 'Editar criterio';
-        }
-        if(currentMode == 'update_modal_descripcion'){
-            document.getElementById("customModalLabel").innerText = 'Editar descripcion';
-        }
-
-        if (currentMode === 'descripcion') {
-
-            document.getElementById("customModalLabel").innerText = 'Agregar nueva descripcion';
-        }
-        if (currentMode === 'level') {
-
-            document.getElementById("customModalLabel").innerText = 'Agregar nuevo nivel';
-        }
-        if (currentMode === 'criterion') {
-
-            document.getElementById("customModalLabel").innerText = 'Agregar nuevo criterio';
-        }
-
-        // Show Bootstrap modal
-        $('#customModal').modal('show');
+    }
+    if(currentMode == 'update_modal_level'){
+        document.getElementById("customModalLabel").innerText = 'Editar nivel';
+    }
+    if(currentMode == 'update_modal_descripcion_criterio'){
+        document.getElementById("customModalLabel").innerText = 'Editar criterio';
+    }
+    if(currentMode == 'update_modal_descripcion'){
+        document.getElementById("customModalLabel").innerText = 'Editar descripcion';
     }
 
-    function submitCustomModal() {
+    if (currentMode === 'descripcion') {
 
-        if (currentMode === 'criterion') {
-            const criterionDesc = document.getElementById('criterionDesc').value;
-            const rubrica = document.getElementById('rubrica').value;
-            const data = {
-                descripcion: criterionDesc,
-                rubrica_actividad_id: rubrica
-            };
+        document.getElementById("customModalLabel").innerText = 'Agregar nueva descripcion';
+    }
+    if (currentMode === 'level') {
 
+        document.getElementById("customModalLabel").innerText = 'Agregar nuevo nivel';
+    }
+    if (currentMode === 'criterion') {
 
-
-
-
-
-            EnviarInformacion('/admin/criterios', data, 'POST', function(response) {
-                addCriterion(response.descripcion, response.id); // Asegúrate de manejar la respuesta
-
-
-            });
-        } else if (currentMode === 'level') {
-            const levelDesc = document.getElementById('levelDesc').value;
-            const scoreRangeFrom = document.getElementById('scoreRangeFrom').value;
-
-            const rubrica1 = document.getElementById('rubrica').value;
-
-            const data = {
-                nombre: levelDesc,
-                puntos: scoreRangeFrom,
-                rubrica_actividad_id: rubrica1,
-            };
-
-
-
-            EnviarInformacion('/admin/niveles', data,'POST', function(response) {
-                window.location.reload();
-
-                addLevel(response.nombre); // Asegúrate de manejar la respuesta
-            });
-        } else if( currentMode == 'update_modal_level'){
-            const edit_level = document.getElementById('edit_level').value;
-            const scoreRangeFromEdit =document.getElementById('scoreRangeFromEdit').value;
-            const data = {
-                nombre: edit_level,
-                puntos: scoreRangeFromEdit,
-            }
-            EnviarInformacion('/admin/niveles/'+ nivel, data, 'PUT', function(response) {
-                window.location.reload();
-
-                addLevel(response.nombre); // Asegúrate de manejar la respuesta
-            });
-
-        } else if( currentMode == 'update_modal_descripcion_criterio'){
-            const edit_d_criterio = document.getElementById('edit_d_criterio').value;
-
-            const data = {
-                descripcion:  edit_d_criterio,
-
-            }
-            EnviarInformacion('/admin/criterios/'+ criterio, data, 'PUT', function(response) {
-                window.location.reload();
-
-                addLevel(response.nombre); // Asegúrate de manejar la respuesta
-            });
-
-        }else if( currentMode == 'update_modal_descripcion'){
-            const edit_descripcion_nivel = document.getElementById('edit_descripcion_nivel').value;
-            const edit_criterio_id = document.getElementById('edit_criterio_id').value;
-            const edit_nivel_id = document.getElementById('edit_nivel_id').value;
-
-            console.log(edit_criterio_id, edit_descripcion_nivel, edit_nivel_id);
-            const data = {
-                descripcion:  edit_descripcion_nivel,
-                criterio_id: edit_criterio_id,
-                nivel_desempeno_id: edit_nivel_id,
-
-            }
-            EnviarInformacion('/admin/descripcionacriterionivel/'+  descripcion, data, 'PUT', function(response) {
-                window.location.reload();
-
-                addLevel(response.nombre); // Asegúrate de manejar la respuesta
-            });
-
-        } else if (currentMode === 'descripcion'){
-
-            const descripcion_input = document.getElementById('descripcion_input').value;
-
-            const data = {
-                descripcion: descripcion_input,
-                nivel_desempeno_id: nivel,
-                criterio_id: criterio
-            }
-
-
-
-
-            EnviarInformacion('/admin/descripcionacriterionivel', data,'POST', function(response) {
-                window.location.reload();
-
-
-                addDescription(criterio, nivel, response.descripcion); // Asegúrate de manejar la respuesta
-            });
-
-
-
-
-
-
-
-        }else if(currentMode === 'calificar'){
-            const estudiante_id = document.getElementById('estudiante').value;
-            const actividad_id = document.getElementById('actividad').value;
-            const CalificarDesc = document.getElementById('CalificarDesc').value;
-
-            const data = {
-                estudiante_id: estudiante_id,
-                actividad_id: actividad_id,
-                nota: CalificarDesc
-            }
-
-
-
-            EnviarInformacion('/admin/valoracion', data, 'POST', function(response) {
-                window.location.reload();
-            });
-
-
-        }
+        document.getElementById("customModalLabel").innerText = 'Agregar nuevo criterio';
     }
 
-    function EnviarInformacion(url, objEvento, type, callback) {
-        $.ajax({
-            type: type,
-            url: url,
-            data: objEvento,
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            },
-            success: function(msg) {
-                console.log(msg);
-                if (callback) callback(msg);
-                var modal = bootstrap.Modal.getInstance(document.getElementById('customModal'));
-                modal.hide();
-            },
-            error: function(xhr, status, error) {
-                alert("Completa todos los campos");
-            }
+    // Show Bootstrap modal
+    $('#customModal').modal('show');
+}
+
+function submitCustomModal() {
+
+    if (currentMode === 'criterion') {
+        const criterionDesc = document.getElementById('criterionDesc').value;
+        const rubrica = document.getElementById('rubrica').value;
+        const data = {
+            descripcion: criterionDesc,
+            rubrica_actividad_id: rubrica
+        };
+
+
+
+
+
+
+        EnviarInformacion('/admin/criterios', data, 'POST', function(response) {
+            addCriterion(response.descripcion, response.id); // Asegúrate de manejar la respuesta
+
 
         });
+    } else if (currentMode === 'level') {
+        const levelDesc = document.getElementById('levelDesc').value;
+        const scoreRangeFrom = document.getElementById('scoreRangeFrom').value;
+
+        const rubrica1 = document.getElementById('rubrica').value;
+
+        const data = {
+            nombre: levelDesc,
+            puntos: scoreRangeFrom,
+            rubrica_actividad_id: rubrica1,
+        };
+
+
+
+        EnviarInformacion('/admin/niveles', data,'POST', function(response) {
+            window.location.reload();
+
+            addLevel(response.nombre); // Asegúrate de manejar la respuesta
+        });
+    } else if( currentMode == 'update_modal_level'){
+        const edit_level = document.getElementById('edit_level').value;
+        const scoreRangeFromEdit =document.getElementById('scoreRangeFromEdit').value;
+        const data = {
+            nombre: edit_level,
+            puntos: scoreRangeFromEdit,
+        }
+        EnviarInformacion('/admin/niveles/'+ nivel, data, 'PUT', function(response) {
+            window.location.reload();
+
+            addLevel(response.nombre); // Asegúrate de manejar la respuesta
+        });
+
+    } else if( currentMode == 'update_modal_descripcion_criterio'){
+        const edit_d_criterio = document.getElementById('edit_d_criterio').value;
+
+        const data = {
+            descripcion:  edit_d_criterio,
+
+        }
+        EnviarInformacion('/admin/criterios/'+ criterio, data, 'PUT', function(response) {
+            window.location.reload();
+
+            addLevel(response.nombre); // Asegúrate de manejar la respuesta
+        });
+
+    }else if( currentMode == 'update_modal_descripcion'){
+        const edit_descripcion_nivel = document.getElementById('edit_descripcion_nivel').value;
+        const edit_criterio_id = document.getElementById('edit_criterio_id').value;
+        const edit_nivel_id = document.getElementById('edit_nivel_id').value;
+
+        console.log(edit_criterio_id, edit_descripcion_nivel, edit_nivel_id);
+        const data = {
+            descripcion:  edit_descripcion_nivel,
+            criterio_id: edit_criterio_id,
+            nivel_desempeno_id: edit_nivel_id,
+
+        }
+        EnviarInformacion('/admin/descripcionacriterionivel/'+  descripcion, data, 'PUT', function(response) {
+            window.location.reload();
+
+            addLevel(response.nombre); // Asegúrate de manejar la respuesta
+        });
+
+    } else if (currentMode === 'descripcion'){
+
+        const descripcion_input = document.getElementById('descripcion_input').value;
+
+        const data = {
+            descripcion: descripcion_input,
+            nivel_desempeno_id: nivel,
+            criterio_id: criterio
+        }
+
+
+
+
+        EnviarInformacion('/admin/descripcionacriterionivel', data,'POST', function(response) {
+            window.location.reload();
+
+
+            addDescription(criterio, nivel, response.descripcion); // Asegúrate de manejar la respuesta
+        });
+
+
+
+
+
+
+
+    }else if(currentMode === 'calificar'){
+        const estudiante_id = document.getElementById('estudiante').value;
+        const actividad_id = document.getElementById('actividad').value;
+        const CalificarDesc = document.getElementById('CalificarDesc').value;
+
+        const data = {
+            estudiante_id: estudiante_id,
+            actividad_id: actividad_id,
+            nota: CalificarDesc
+        }
+
+
+
+        EnviarInformacion('/admin/valoracion', data, 'POST', function(response) {
+            window.location.reload();
+        });
+
+
     }
 }
 
-function EnviarInformacion(url, objEvento, callback) {
+function EnviarInformacion(url, objEvento, type, callback) {
     $.ajax({
-        type: 'POST',
+        type: type,
         url: url,
         data: objEvento,
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         },
         success: function(msg) {
+            console.log(msg);
             if (callback) callback(msg);
             var modal = bootstrap.Modal.getInstance(document.getElementById('customModal'));
             modal.hide();
@@ -262,6 +231,8 @@ function addCriterion(description, criterio) {
     }
 }
 
+
+
 function addLevel(name) {
     const headerRow = document.getElementById('headerRow');
     const newHeaderCell = headerRow.insertCell(-1);
@@ -285,6 +256,7 @@ function addDescription(criterioId, nivelId, description) {
 
         // Busca la fila correcta por criterio ID
         if (row.getAttribute('data-criterio-id') == criterioId) {
+
             // Encuentra la celda correcta por nivel ID
             const cells = row.cells;
             for (let j = 1; j < cells.length; j++) {  // Empezamos en 1 porque la primera celda es la descripción del criterio
@@ -300,14 +272,10 @@ function addDescription(criterioId, nivelId, description) {
     }
 }
 
-function addCalificacion(name) {
-    document.getElementById('CalificarDesc').value = 4;
+function addCalificacion(name){
+ document.getElementById('CalificarDesc').value = 4;
+
 }
 
-// Función para hacer que el botón de cancelar cierre el modal
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('cancelButton').addEventListener('click', function () {
-        var modal = bootstrap.Modal.getInstance(document.getElementById('customModal'));
-        modal.hide();
-    });
-});
+
+
